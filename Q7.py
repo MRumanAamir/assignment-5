654038efd0e0b93f9d6e5f069ca3b90e3071f442
rumanaamir <rumanaamir2022@gmail.com>
2024-09-08 06:59:53 +0000

    Q7

--- Q7.py
+++ Q7.py
@@ -1,1 +1,6 @@
-hbhbhbjbjn
+year = int(input("Enter an year: "))
+
+if(year%4==0 and year%100!=0) or (year%400==0):
+    print(f"{year} is a leap year. ")
+else:
+    print(f"{year} is not a leap year. ")
